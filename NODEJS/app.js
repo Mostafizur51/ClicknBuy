af3eb7b343759f341b12963c5d0d5751dab0b959
Mostafizur51 <mrmostafizur22@gmail.com>
2023-05-15 15:41:08 +0600

    I EDIT BACK

--- NODEJS/app.js
+++ NODEJS/app.js
@@ -1,6 +1,41 @@
-const myEmitter=new myEmitter();
-myEmitter.on('event',function(a,b){ 
 
-    console.log(a,b,this==myEmitter);
-});
-myEmitter.emit('event','a','b');
+let xp=0;
+let health=100;
+let gold=50;
+
+const btn=document.querySelector(".btn");
+const text=document.querySelector(".col-2");
+
+const location=[
+    { 
+    name:"town squre",
+    "button text":["go to store"],
+    "button functions":[goStore],
+    text:"you Enter the store"
+}
+{ 
+    name:"Store",
+    "button text":["buy this"],
+    "button functions":[goStore],
+    text:"you Enter the store"
+}
+]
+
+btn.onclick=goStore;
+
+
+function goStore(){ 
+ btn.innerText="Nothing";
+ text.innerText="there are nothing but you are so what should i do";
+}
+function goTown(){ 
+     
+    console.log("None with what");
+    btn.onclick=goStore;
+
+}
+
+
+
+
+
